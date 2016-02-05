689dd2734d2c830b1af9694b97ececfd01c1a21a
Ryan O'Connor <charle692@gmail.com>
2016-02-05 00:55:03 -0500

    Printed out contents of inverted index to console

--- src/test/Test_Indexer.java
+++ src/test/Test_Indexer.java
@@ -1,16 +1,29 @@
 package test;
 
 import java.util.HashMap;
+import java.util.Map.Entry;
+
 import indexing.*;
 
 public class Test_Indexer {
+	
+	/**
+	 * This just creates an inverted index and prints out the contents to console for testing
+	 * 
+	 * @param args
+	 */
 	public static void main(String[] args) {
 		Custom_Indexing customIndexer = new Custom_Indexing();
 		customIndexer.createInvertedIndex();
 		
 		HashMap<String, HashMap<Integer, Integer>> invertedIndex = customIndexer.getInvertedIndex();
-		invertedIndex.forEach((k,v)->System.out.println(k));
-		System.out.println("Total number of tokens in the file: " + customIndexer.getNumberOfTokens());
-		System.out.println("Total number of tokens in the inverted index: " + invertedIndex.size());
+		
+		System.out.println("{");
+		for(Entry<String, HashMap<Integer, Integer>> entry : invertedIndex.entrySet()) {
+		    System.out.print("  " + entry.getKey() + ": {");
+		    entry.getValue().forEach((k,v)->System.out.print(k + ": " + v + ", "));
+		    System.out.print("}\n");
+		}
+		System.out.println("}");
 	}
 }
